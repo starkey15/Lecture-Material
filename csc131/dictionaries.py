008e28d882f70ef5d4d105f20554f9e7c510c7bc
Colton Starkey <starkey15@live.missouristate.edu>
2018-01-29 13:13:10 -0600

    dictionaries from 1-29-2018

--- csc131/dictionaries.py
+++ csc131/dictionaries.py
@@ -5,3 +5,29 @@ def get_personal_data() -> dict:
     personal_data = {"name": "Colton", "role": "student"}
     return personal_data
 
+def main() -> int:
+    default_dict = dict() #default because no arguments
+    print(default_dict)
+
+    initialized_dict = dict([('name', 'Colton'), ('role', 'student')]) #easy for string arguments
+    print(initialized_dict)
+
+    simple_init_dict = dict(name = 'Colton', role = 'student') #can use if you don't necessarily want strings
+    print(simple_init_dict)
+
+    simple_init_dict['role'] = '27' #dictionaries are mutable
+    print(simple_init_dict)
+
+    my_comprehension = {x: x**2 for x in range(1,6)} #arbitrarily assign key value pairs (1,2,3,4,5)
+    print(my_comprehension)
+
+    my_comprehension = {x: x ** 2 for x in range(5)} #for index range up to but not including 5 (0-4)
+    print(my_comprehension)
+
+    s = "little,".translate({ord(i): None for i in string.punctuation})
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
