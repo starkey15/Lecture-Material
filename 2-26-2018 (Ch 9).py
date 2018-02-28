8a2e40f2cff19d260734bd028ccaf8f91c2b0925
Colton Starkey <starkey15@live.missouristate.edu>
2018-02-28 12:52:21 -0600

    Light changes for 2-28-2018

--- 2-26-2018 (Ch 9).py
+++ 2-26-2018 (Ch 9).py
@@ -2,11 +2,6 @@
 class varible - variable that works for all methods of a class
 """
 
-"""
-File: student.py
-Resources to manage a student's name and test scores.
-"""
-
 
 class Student(object):
     """Represents a student."""
@@ -79,17 +74,7 @@ def main():
     print("{}'s highest score is {}.".format(student.get_name(), student.get_high_score()))
     print("{}'s first score was {}.".format(student.get_name(), student.get_score(1)))
     help(Student)
-
-
-if __name__ == "__main__":
-    main()
-
-#------------------------------------------------------------------------------#
-
-"""
-File: rational.py
-Resources to manipulate rational numbers.
-"""
+# --------------------------------------------------#
 
 
 class Rational(object):
@@ -202,7 +187,80 @@ def main():
     print("one_half + one_sixth =  {}".format(one_half + one_sixth))
     print("one_half == one_sixth = {}".format(one_half == one_sixth))
     print("one_half >= one_sixth = {}".format(one_half >= one_sixth))
+# --------------------------------------------------#
 
 
-if __name__ == "__main__":
-    main()
+class SavingsAccount:
+    """This class represents a savings account with the owner's name, PIN, and balance."""
+
+    RATE = 0.02  # Single rate for all accounts (class variable)
+
+    def __init__(self, name: str, pin: str, balance: float = 0.0):
+        self._name = name
+        self._pin = pin
+        self._balance = balance
+
+    def __str__(self) -> str:
+        """
+        Returns the string rep.
+        :return A string representation of this SavingsAccount is returned.
+        """
+        result = 'Name:    ' + self._name + '\n'
+        result += 'PIN:     ' + self._pin + '\n'
+        result += 'Balance: ' + str(self._balance)
+        return result
+
+    def get_balance(self) -> float:
+        """
+        Returns the current balance.
+        :return The current balance of this SavingsAccount is returned.
+        """
+        return self._balance
+
+    def get_name(self) -> str:
+        """
+        Returns the current name.
+        :return The name of the person who owns this SavingsAccount is returned.
+        """
+        return self._name
+
+    def get_pin(self) -> str:
+        """
+        Returns the current pin.
+        :return The pin number of this SavingsAccount is returned.
+        """
+        return self._pin
+
+    def deposit(self, amount) -> None:
+        """
+        If the amount is valid, adds it to the balance and returns None; otherwise, returns an error message.
+        :param amount: The amount to deposit into this SavingsAccount
+        :return None
+        """
+        self._balance += amount
+        return None
+
+    def withdraw(self, amount):
+        """
+        If the amount is valid, subtract it from the balance and returns None; otherwise, returns an error message.
+        :param amount: The amount to withdraw from this SavingsAccount
+        :return An error message is returned if the amount is less than 0 or if there are insufficient funds; otherwise
+        None is returned.
+        """
+        if amount < 0:
+            return "Amount must be >= 0"
+        elif self._balance < amount:
+            return "Insufficient funds"
+        else:
+            self._balance -= amount
+            return None
+
+    def compute_interest(self) -> float:
+        """
+        Computes, deposits, and returns the interest.
+        :return The amount of interest computed and deposited is returned.
+        """
+        interest = self._balance * SavingsAccount.RATE
+        self.deposit(interest)
+        return interest
+# --------------------------------------------------#
